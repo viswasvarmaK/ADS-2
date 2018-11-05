5c1ef92645e182ed134fffa20344c02c9724275b
viswasvarmasatya <viswasvarmasatya@msitprogram.net>
2018-11-05 19:09:02 +0530

    "ads2_m3_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ m3/3.1 Directed Cycle/3.1 Directed Cycle/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int vertices = scan.nextInt();
+		int edges = scan.nextInt();
+		scan.nextLine();
+		
+	}
+}
